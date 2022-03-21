813d4326b424ec1a0cdc5587a6ed3dce07a0d04c
delaquash <olaide1191@gmail.com>
2022-03-21 04:44:04 +0100

    implemented initial layout and folder structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { CssBaseline, Grid } from '@material-ui/core';
+
+
+import Header from './component/Header/Header';
+import List from './component/List/list';
+import Map from './component/Map/map';
+import PlaceDetails from './component/PlaceDetails/placedetails';
 
 const App = () => {
   return (
-    <div>
-        Welcome to React, Emmanuel
-    </div>
+    <>
+    <CssBaseline />
+    <Header />
+    <Grid container spacing={3} style={{ width: '100%' }}>
+        <Grid item xs={12} md={4}>
+            <List />
+        </Grid>
+        <Grid item xs={12} md={4}>
+            <Map />
+        </Grid>
+    </Grid>
+    </>
   )
 }
 
---
+++ src/component/PlaceDetails/PlaceDetails.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function PlaceDetails() {
+  return (
+    <div>This is the place details section</div>
+  )
+}
